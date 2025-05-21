663c36f38abb906d88ce42cf5a868222e67869ad
Miltchola <miltonkiefermello@gmail.com>
2025-05-20 23:10:56 -0300

    Feat: Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,39 @@
+import './Footer.css'
+
+import insta from '../../assets/icons/Instagram logo.png'
+import webtoon from '../../assets/icons/Webtoon logo.png'
+
+import logo from '../../assets/icons/Teste Logo - Guardians (branca).png'
+import logoTexto from '../../assets/icons/Logo - Guardians texto.png'
+
+
+const Footer = () => {
+    return (
+        <div className="footer-section">
+            <div className="social-media">
+                <a className="social-media-link" href="https://www.instagram.com/the.guardians.official/">
+                    <img className="logo" src={insta} alt="Instagram Logo" />
+                    <h4 className="media-text">@the.guardians.official</h4>
+                </a>
+                
+                <a className="social-media-link" href="https://www.webtoons.com/en/canvas/the-guardians-protectors-of-mankind/list?title_no=702490">
+                    <img className="logo" src={webtoon} alt="Webtoon Logo" />
+                    <h4 className="media-text">The Guardians - Protectors of Mankind</h4>
+                </a>
+            </div>
+
+            <div className="footer-logo">
+                <img className="logo-icon" src={logo} alt="logo" />
+                <img className="logo-text" src={logoTexto} alt="logoTexto" />
+            </div>
+
+            <hr className="hr-section" />
+
+            <div className="footer-rights">
+                <p>&copy; 2025 The Guardians - Protectors of Mankind. All rights reserved.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
